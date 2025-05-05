e8e3a2156d1426d54ff56bb79bf8a44bf2d8a13c
shyamp1-byte <shyamie123@gmail.com>
2025-05-05 13:50:52 -0400

    Create Expense class with category support

--- Expense.java
+++ Expense.java
@@ -1,3 +1,18 @@
-public class Expense {
-    
+public class Expense extends Entry {
+    private String category;
+
+    public Expense(double amount, String category) {
+        super(amount);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense (" + category + "): $" + amount;
+    }
 }
+
