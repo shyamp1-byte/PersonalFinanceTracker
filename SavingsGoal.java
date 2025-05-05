87bbfcacd680822bfbd2ec617b24204df69e71d7
shyamp1-byte <shyamie123@gmail.com>
2025-05-05 14:52:25 -0400

    Add SavingsGoal class with target amount and deadline

--- SavingsGoal.java
+++ SavingsGoal.java
@@ -1,3 +1,20 @@
+import java.util.Date;
+
 public class SavingsGoal {
-    
+    private double targetAmount;
+    private Date deadline;
+
+    public SavingsGoal(double targetAmount, Date deadline) {
+        this.targetAmount = targetAmount;
+        this.deadline = deadline;
+    }
+
+    public double getTargetAmount() {
+        return targetAmount;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
 }
+
