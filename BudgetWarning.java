ec5d25c045082e42ea4cf279c72133a0dd584034
shyamp1-byte <shyamie123@gmail.com>
2025-05-05 18:26:10 -0400

    Implement BudgetWarning class to monitor overspending

--- BudgetWarning.java
+++ BudgetWarning.java
@@ -1,3 +1,15 @@
 public class BudgetWarning {
-    
+    private double limit;
+
+    public BudgetWarning(double limit) {
+        this.limit = limit;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public boolean isOverLimit(double amountSpent) {
+        return amountSpent > limit;
+    }
 }
