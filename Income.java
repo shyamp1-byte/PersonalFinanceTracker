aa6dd497b45ef080c78c3b9ab7aae0f1602d846a
shyamp1-byte <shyamie123@gmail.com>
2025-05-05 13:27:41 -0400

    Create Income class extending Entry

--- Income.java
+++ Income.java
@@ -1,3 +1,18 @@
-public class Income {
-    
+public class Income extends Entry {
+    private String source;
+
+    public Income(double amount, String source) {
+        super(amount);
+        this.source = source;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    @Override
+    public String toString() {
+        return "Income from " + source + ": $" + amount;
+    }
 }
+
